6d210714932cb304b28a03a7a3eafd78ad7f0666
Lynn <59608151+OrangeRed0706@users.noreply.github.com>
2023-07-08 16:11:45 +0800

    Update install.sh

--- Linux/Ubuntu/install.sh
+++ Linux/Ubuntu/install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# Update the source.list
+# Update the source list
 sudo apt-get update
-sudo apt-get install -y vim git cmake build-essential tcpdump tig socat bash-completion golang libpcap-dev bridge-utils ipcalc conntrack
+
+# Install required packages
+sudo apt-get install -y vim git cmake build-essential tcpdump tig socat bash-completion golang libpcap-dev bridge-utils ipcalc conntrack ca-certificates curl gnupg
 
 # Install Docker
-export DOCKER_VERSION="5:19.03.15~3-0~ubuntu-focal"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
-sudo apt-get install -y docker-ce=${DOCKER_VERSION}
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add current user to docker group, might require logout and login to take effect
 sudo usermod -aG docker $USER
 
-git clone https://github.com/hwchiu/hiskio-course.git
-
+# Clone and install bash-it
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 bash ~/.bash_it/install.sh -s
-
-# Install tcpterm
-go get -d github.com/sachaos/tcpterm
-pushd go/src/github.com/sachaos/tcpterm/
-go install
-popd
