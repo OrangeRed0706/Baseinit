d7dacb40657177dfa2c40b2ec8f74fb60bb7db56
Lynn <59608151+OrangeRed0706@users.noreply.github.com>
2023-07-08 16:13:25 +0800

    Update install.sh

--- Linux/Ubuntu/install.sh
+++ Linux/Ubuntu/install.sh
@@ -10,10 +10,13 @@ sudo apt-get install -y vim git cmake build-essential tcpdump tig socat bash-com
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Extract architecture and Ubuntu version
+ARCHITECTURE=$(dpkg --print-architecture)
+VERSION_CODENAME=$(. /etc/os-release && echo "$VERSION_CODENAME")
+
+# Create docker list file with extracted variables
+echo "deb [arch=$ARCHITECTURE signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $VERSION_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
